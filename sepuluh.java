11cf713e74d54c8032c92161881c276c42a4b51a
Istiana <istiana.ubudiy@gmail.com>
2015-03-25 19:01:55 -0700

    sepuluh
    ini nomer sepuluh

---
+++ sepuluh.java
@@ -0,0 +1,17 @@
+/**
+ * Created by USER on 3/25/2015.
+ */
+public class sepuluh {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c=3;
+
+        for(a=1;a<=c;a++){
+            for(b=1;b<=c;b++){
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+    }
+}
